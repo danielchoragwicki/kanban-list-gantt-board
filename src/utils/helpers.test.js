a2f4e2be2beb5d0eae02fd5e4c389a6fa44b3f62
daniel <daniel@flipside.com>
2019-03-02 00:40:02 +0100

    unit tests

--- src/utils/helpers.test.js
+++ src/utils/helpers.test.js
@@ -1,18 +1,128 @@
 import { generateId, findById, addItem, updateList, removeItem, truncate, reorder, move } from './helpers';
 
-// test('addItem should add the passed item to the list', () => {
-//     const startItems = [
-//       {id:1, name: 'one', isComplete: false},
-//       {id:2, name: 'two', isComplete: false}
-//     ]
-//     const newItem = {id:3, name: 'three', isComplete: false}
-//     const expected = [
-//       {id:1, name: 'one', isComplete: false},
-//       {id:2, name: 'two', isComplete: false},
-//       {id:3, name: 'three', isComplete: false}
-//     ]
+//generateId
+
+test('generateIdFunctionIsString_ReturnTrue', () => {
+    const id = generateId();
+    const result = typeof id === 'string';
+
+    expect(result).toBeTruthy();
+})
+
+test('generateIdFunctionIsNotString_ReturnFalse', () => {
+    const id = generateId();
+    const result = typeof id !== 'string';
+
+    expect(result).toBeFalsy();
+})
+
+//findById
+
+test('findByIdFunction_ReturnExpectedItemFromArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const expected = {id:2, name: 'two', lists: []}
+    const result = findById(2, startBoards)
+
+    expect(result).toEqual(expected)
+})
+
+//addItem
+
+test('addItemAddThePasseItemToTheList_ReturnExpectedArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []}
+    ]
+    const newItem = {id:3, name: 'three', lists: []}
+    const expected = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const result = addItem(startBoards, newItem)
+
+    expect(result).toEqual(expected)
+})
+   
+test('addItemNotMutateTheExistingItemsList_ReturnNotMutateArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []}
+    ]
+    const newItem = {id:3, name: 'three', lists: []}
+    const result = addItem(startBoards, newItem)
   
-//     const result = addItem(startTodos, newTodo)
+    expect(result).not.toBe(startBoards)
+})
+
+//updateList
+
+test('updateTodoFunctionUpdateItemById_ReturnExpectedArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const updatedBoards = {id:2, name: 'two', lists: ['test_list']}
+    const expectedBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: ['test_list']},
+        {id:3, name: 'three', lists: []}
+    ]
+    const result = updateList(startBoards, updatedBoards)
+
+    expect(result).toEqual(expectedBoards)
+})
   
-//     expect(result).toEqual(expected)
-//   })
+test('updateTodoNotMutateTheOriginalArray_ReturnNotMutateArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const updatedBoards = {id:2, name: 'two', lists: ['test_list']}
+    const result = updateList(startBoards, updatedBoards)
+
+    expect(result).not.toBe(startBoards)
+})
+  
+
+//removeItem
+
+test('removeItemFunctionRemoveItemById_ReturnExpectedArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const targetId = 2;
+    const expected = [
+        {id:1, name: 'one', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const result = removeItem(startBoards, targetId)
+
+    expect(result).toEqual(expected)
+})
+  
+test('removeItemNotMutateTheExistingItemsList_ReturnNotMutateArray', () => {
+    const startBoards = [
+        {id:1, name: 'one', lists: []},
+        {id:2, name: 'two', lists: []},
+        {id:3, name: 'three', lists: []}
+    ]
+    const targetId = 2;
+    const result = removeItem(startBoards, targetId)
+
+    expect(result).not.toBe(startBoards)
+})
+
+//truncate
+
+//reorder
+
+//move
