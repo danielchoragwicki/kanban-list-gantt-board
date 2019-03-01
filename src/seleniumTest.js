10fcb102a9e340c50ea58cfc985912c5e9dd34b9
daniel <daniel@flipside.com>
2019-03-01 22:46:26 +0100

    final selenium

--- src/seleniumTest.js
+++ src/seleniumTest.js
@@ -1,13 +1,51 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
+const {
+    Builder,
+    By,
+    Key
+} = require('selenium-webdriver');
 require("chromedriver");
 
-(async function example() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
-  } finally {
-    await driver.quit();
-  }
+(async function selenium() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    async function clickFunc (className) {
+        const element = await driver.findElement(By.className(className));
+        await element.click();
+        await driver.sleep(700);
+    }
+
+    async function sendKeysFunc (className, keys, key) {
+        const element = await driver.findElement(By.className(className));
+        key ? await element.sendKeys(keys, key) : await element.sendKeys(keys);
+        await driver.sleep(700);
+    }
+
+    try {
+        await driver.get('http://localhost:3000/');
+
+        await clickFunc('board-item__link--add');
+
+        await sendKeysFunc("addboard__input", "New board");
+
+        await clickFunc('night-fade');
+
+        await clickFunc('addboard__submit');
+
+        await clickFunc('board-item__link night-fade');
+
+        await sendKeysFunc("new-list__input", 'New list', Key.ENTER);
+
+        await sendKeysFunc('new-card__input', 'New list', Key.ENTER);
+
+        await clickFunc('card__edit-button');
+
+        await sendKeysFunc('card-edit__desc', 'New desc');
+
+        await clickFunc('button--save');
+
+        // await sendKeysFunc('new-list__input', 'New list', Key.ENTER);
+
+    } finally {
+        await driver.sleep(2000);
+        await driver.quit();
+    }
 })();
